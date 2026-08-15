649c95e8bb77b18d5cf48d6f9029e402e99d0d3f
regionalcflmis <regionalcflmis@gmail.com>
2026-08-15 19:16:20 +0530

    Update api.js

--- api.js
+++ api.js
@@ -17,7 +17,7 @@
  */
 
 // ⬇️ PASTE YOUR APPS SCRIPT WEB APP /exec URL HERE (see README step 2)
-var API_URL = 'https://script.google.com/macros/s/AKfycbxDveVEj52wG7Owk5icdeMzEiPWrjJtAOjYF8jt84d-HucFfqwxGOZYO-3919jmgU47Lw/exec';
+var API_URL = 'https://script.google.com/macros/s/AKfycbxDveVEj52wG7Owk5icdeMzEiPWrjJtAOjYF8jt84d-HucFfqwxGOZYO-3919jmgU47Lw/exec?page=form';
 
 (function () {
   function callServer(fnName, args) {
