f69ca5fdfccee167c898fe1a40874e58cef20547
Vivek Mishra <regionalcflmis@gmail.com>
2026-08-21 11:26:09 +0530

    Add files via upload

--- api.js
+++ api.js
@@ -17,7 +17,7 @@
  */
 
 // ⬇️ PASTE YOUR APPS SCRIPT WEB APP /exec URL HERE (see README step 2)
-var API_URL = 'https://script.google.com/macros/s/AKfycbxyBg-zMCCYjpWTgGunyQ4sifsgPRR5NJRUBHc9g4m5waZgnFlw6wHgAqumSKBk7Si-Gg/exec';
+var API_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
 
 (function () {
   function callServer(fnName, args) {
@@ -58,6 +58,11 @@ var API_URL = 'https://script.google.com/macros/s/AKfycbxyBg-zMCCYjpWTgGunyQ4sif
     'checkLoginExempt',
     'getAdminEmployeeList',
     'getCascadingMasterData',
+    'getAdminUsersList',
+    'adminAddAdminUser',
+    'adminRemoveAdminUser',
+    'getComplianceTracker',
+    'sendComplianceReminder',
     'adminUpdateEmployee',
     'getEmployeeAssignedCFLs',
     'getMySubmissions',
